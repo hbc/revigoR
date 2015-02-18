1a4ae12e3312b961f7bc2bf3f315b0ffbb8d2808
John Hutchinson <jnhutchinson@gmail.com>
2015-02-18 15:45:57 -0500

    first commit

---
+++ revigo_example.R
@@ -0,0 +1,8 @@
+GOs <- c("GO:0009268","GO:0010447","GO:0000027","GO:0042255","GO:0042257","GO:0042273","GO:0030880","GO:0009775","GO:0009853","GO:0030255")
+pvals <- rep(0.01, length(GOs))
+
+mycommand=paste('~/Scripts/revigo.pl -goterms', paste(GOs,collapse=","), '-gopvals', paste(pvals,collapse=","), sep=" ")
+
+mytempfile <- tempfile()
+system2(command='perl', args=mycommand, stdout=mytempfile)
+source(mytempfile)
